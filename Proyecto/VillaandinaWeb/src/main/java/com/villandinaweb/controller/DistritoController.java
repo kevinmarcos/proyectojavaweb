544071500b8ea31b64c4a57eb4876cead72a3064
kevinmarcos <81602436+kevinmarcos@users.noreply.github.com>
2022-10-26 08:46:28 -0500

    3rd commit

---
+++ Proyecto/VillaandinaWeb/src/main/java/com/villandinaweb/controller/DistritoController.java
@@ -0,0 +1,57 @@
+
+package com.villandinaweb.controller;
+
+import com.villandinaweb.entity.DistritoEntity;
+import com.villandinaweb.service.DistritoService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/distrito")
+public class DistritoController {
+    @Autowired
+    private DistritoService  distritoservice;
+    
+    @GetMapping
+    public List<DistritoEntity> findAll(){
+        return distritoservice.findAll();
+    }
+    
+    @GetMapping("/custom")
+    public List<DistritoEntity> findAllCustom(){
+        return distritoservice.findAll();
+    }
+    
+    @GetMapping("/(id)")
+    public Optional<DistritoEntity> findById(@PathVariable Long id){
+        return distritoservice.findById(id);
+    }
+    
+    @PostMapping
+    public DistritoEntity add(@RequestBody DistritoEntity d){
+        return distritoservice.add(d);
+    }
+    
+    @PutMapping("/(id)")
+    public DistritoEntity update(@PathVariable Long id, @RequestBody DistritoEntity d){
+        d.setCodigo(id);
+        return distritoservice.update(d);
+    }
+    
+    @DeleteMapping
+    public DistritoEntity delete(@PathVariable Long id, @RequestBody DistritoEntity d){
+        DistritoEntity objcategoria = new DistritoEntity();
+        objcategoria.setEstado(false);
+        return distritoservice.delete(DistritoEntity.builder().codigo(id).build());
+    }
+}
