e50b4a3bac6f91502d10a43de7467f2d2c9577ec
Thony-Leca <83093101+Thony-Leca@users.noreply.github.com>
2022-11-16 08:45:40 -0500

    update sql

--- DataBase/VillaAndinasql.sql
+++ DataBase/VillaAndinasql.sql
@@ -5,21 +5,27 @@ use villaandina;
 create table Categoria(
 	codcat int primary key auto_increment,
 	nomcat varchar(40) binary character set utf8 collate utf8_bin not null,
-    estado bit default 1 not null
+    estado bit not null
 );
+alter table Categoria modify estado bit;
 
-insert into Categoria(nomcat) values ("Granos Andino"),("Cacao"),("Super Foods");
+insert into Categoria(nomcat,estado) values 
+("Granos Andino",1),
+("Cacao",1),(
+"Super Foods",1);
 select * from Categoria;
 
 -- ---------------------------------------------------------------------------------- 
 create table Destino(
-	codDestino int primary key auto_increment, 
-    nomDestino varchar(100) binary character set utf8 collate utf8_bin not null,
-	codDistrito int not null,
+	coddestino int primary key auto_increment, 
+    nomdestino varchar(100) binary character set utf8 collate utf8_bin not null,
+	coddistrito int not null,
     estado bit default 1 not null,
     foreign key (codDistrito) references Distrito(codDistrito)
 );
-
+insert into Destino(nomdestino,coddistrito,estado) values 
+("ventas",2,1);
+select * from Destino;
 -- ---------------------------------------------------------------------------------- 
 create table Distrito(
 	codDistrito int primary key auto_increment,
@@ -28,13 +34,13 @@ create table Distrito(
 );
 
 select * from Distrito;
-insert into Distrito(nomdistrito) values 
-("San Juan de Lurigancho"),
-("La Molina"),
-("Pueblo Libre"),
-("Barranco"),
-("San Isidro"),
-("Santa Beatriz");
+insert into Distrito(nomdistrito,estado) values 
+("San Juan de Lurigancho",1),
+("La Molina",1),
+("Pueblo Libre",1),
+("Barranco",1),
+("San Isidro",1),
+("Santa Beatriz",1);
 
 -- ----------------------------------------------------------------------------------
 create table Empleado(
@@ -54,24 +60,13 @@ create table Empleado(
 alter table Empleado drop column apellid_pat;
 
 select * from Empleado;
-insert into Empleado(dni,nomemple,apellidPat,apellidMat,telfemple,correoemple,sexemple,coddistritoemple,direccion) values 
-("74053373","Kevin","Zamudio","Espinoza",951951951,"74053373@certus.edu.pe","M",1,"Jr. HolaMundo 1230"),
-("78945612","Anthony","Cesar","Lecarnaque",987654321,"anthony@certus.edu.pe","M",2,"Jr. JavaSpring 1300"),
-("79846513","Yameli","Carbajal","Cuicapuza",945916973,"yameli@certus.edu.pe","F",3,"Jr. Bootstrap 1649"),
-("64875113","Roy","Ventura","De la Rosa",977946031,"roy@certus.edu.pe","M",4,"Jr. Database 1750");
+insert into Empleado(dni,nomemple,apellidPat,apellidMat,telfemple,correoemple,sexemple,coddistritoemple,direccion,estado) values 
+("74053373","Kevin","Zamudio","Espinoza",951951951,"74053373@certus.edu.pe","M",1,"Jr. HolaMundo 1230",1),
+("78945612","Anthony","Cesar","Lecarnaque",987654321,"anthony@certus.edu.pe","M",2,"Jr. JavaSpring 1300",1),
+("79846513","Yameli","Carbajal","Cuicapuza",945916973,"yameli@certus.edu.pe","F",3,"Jr. Bootstrap 1649",1),
+("64875113","Roy","Ventura","De la Rosa",977946031,"roy@certus.edu.pe","M",4,"Jr. Database 1750",1);
 
--- ---------------------------------------------------------------------------------------------------
-create table Entrada_Producto(
-	codEntrada int primary key auto_increment,
-    codEmpleado int not null,
-    codDest int not null,
-    fechaEntrada date not null,
-    estado bit default 1 not null,
-    foreign key (codDest) references Destino(codDestino),
-    foreign key (codEmpleado) references Empleado(codemple)
-);
-
-
+-- --------------------------------------------------------------------------------------------------
 
 -- ------------------------------------------------------------------------------------------------------
 create table Productos(
@@ -80,46 +75,62 @@ create table Productos(
     catprod int not null,
     duraprod date not null,
     cantidad int not null,
-    estado bit default 1 not null,
+    estprod bit default 1 not null,
     foreign key (catprod) references Categoria(codcat)
 );
 
 select * from Productos;
-insert into Productos(nomprod,catprod,duraprod,cantidad) values
-("Quinua Blanca",1,"2025-10-20",1500),
-("Quinua Roja",1,"2024-5-28",2000),
-("Manteca de Cacao",2,"2023-10-1",1000),
-("Pasta de Cacao",2,"2023-11-15",3000),
-("Maca",3,"2026-09-14",1500),
-("Camu Camu",3,"2022-12-20",2000);
+insert into Productos(nomprod,catprod,duraprod,cantidad,estprod) values
+("Quinua Blanca",1,"2025-10-20",50,1),
+("Quinua Roja",1,"2024-5-28",20,1),
+("Manteca de Cacao",2,"2023-10-1",10,1),
+("Pasta de Cacao",2,"2023-11-15",30,1),
+("Maca",3,"2026-09-14",10,1),
+("Camu Camu",3,"2022-12-20",20,1);
 
 -- -------------------------------------------------------------------------------------------------------
 create table Proveedores(
 	codprov int primary key auto_increment,
-    rucProv varchar(20) not null,
+    rucprov varchar(20) not null,
     nombprov varchar(100) binary character set utf8 collate utf8_bin not null,
-    direccionProv varchar(100) not null,
+    direccionprov varchar(100) not null,
     correoprov varchar(100) binary character set utf8 collate utf8_bin not null,
     telefprov int not null,
     estado bit default 1 not null
 );
 
 select * from Proveedores;
-insert into Proveedores(nombprov,rucProv,direccionProv,correoprov,telefprov) values
-("TechNews Co.",12345678910,"Av. Technologi N° 1000","technewsco@gmail.com",957182465),
-("HealthApp Co.",15975314630,"Av. BootCamp N° 2000","healthappco@gmail.com",945916972),
-("Cinestres Co.",14789632530,"Av. VisualStudio Code N° 3000","cinestresco@gmail.com",947914920);
+insert into Proveedores(nombprov,rucprov,direccionprov,correoprov,telefprov,estado) values
+("TechNews Co.","12345678910","Av. Technologi N° 1000","technewsco@gmail.com",957182465,1),
+("HealthApp Co.","15975314630","Av. BootCamp N° 2000","healthappco@gmail.com",945916972,1),
+("Cinestres Co.","14789632530","Av. VisualStudio Code N° 3000","cinestresco@gmail.com",947914920,1);
 
 -- --------------------------------------------------------------------------------------------------------
+create table Entrada_Producto(
+	codEntrada int primary key auto_increment,
+    codEmpleado int not null,
+    coddest int not null,
+    fechaentrada date not null,
+    estado bit default 1 not null,
+    foreign key (codDest) references Destino(codDestino),
+    foreign key (codEmpleado) references Empleado(codemple)
+);
+select * from Entrada_Producto;
+insert into Entrada_Producto(fechaentrada,coddest,codempleado,estado) values
+("2022-10-20",1,2,1);
+
 create table Salida_Producto(
 	codSalida int primary key auto_increment,
-    fechaSalida date not null,
-    codDestino int not null,
-	codEmpleado int not null,    
+    fechasalida date not null,
+    coddestino int not null,
+	codempleado int not null,    
     estado bit default 1 not null,
     foreign key (codDestino) references Destino(codDestino),
     foreign key (codEmpleado) references Empleado(codemple)
 );
+select * from Salida_Producto;
+insert into Salida_Producto(fechasalida,coddestino,codempleado,estado) values
+("2022-10-20",1,2,1);
 
 -- -------------------------------------------------------------------------------------------------------------
 create table Detalle_Entrada(
